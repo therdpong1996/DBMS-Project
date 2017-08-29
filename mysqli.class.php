ead42e901e5ab1058d55eada773043670bc37ef5
Therdpong Khangrang <numlifez@icloud.com>
2017-08-30 02:31:57 +0700

    Update mysqli.class.php

--- mysqli.class.php
+++ mysqli.class.php
@@ -10,7 +10,9 @@
 	$conn = mysqli_connect($_DB['host'], $_DB['username'], $_DB['password'], $_DB['database']);
 	mysqli_set_charset($conn,"utf8");
 
+
 	if ($_POST['temp'] and $_POST['humi']) {
 		$data = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM display_data LIMIT 1"));
+		$data['uptime'] = date('H:i:s d/m/Y');
 		echo json_encode($data);
 	}
