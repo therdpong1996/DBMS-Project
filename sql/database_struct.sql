d927de72366b8bbc2a79c20a5344898f12f26e83
Therdpong Khangrang <numlifez@icloud.com>
2017-08-30 02:33:10 +0700

    Update database_struct.sql

--- sql/database_struct.sql
+++ sql/database_struct.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 08, 2017 at 10:02 PM
+-- Generation Time: Aug 30, 2017 at 02:32 AM
 -- Server version: 5.7.19-0ubuntu0.16.04.1
--- PHP Version: 7.0.18-0ubuntu0.16.04.1
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `admin_dbms`
+-- Database: `database_struct`
 --
 
 -- --------------------------------------------------------
@@ -30,13 +30,6 @@ CREATE TABLE `display_data` (
   `d_temp` float NOT NULL,
   `d_humi` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `display_data`
---
-
-INSERT INTO `display_data` (`d_temp`, `d_humi`) VALUES
-(0, 0);
 
 -- --------------------------------------------------------
 
@@ -51,6 +44,19 @@ CREATE TABLE `log_datas` (
   `time` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mistake`
+--
+
+CREATE TABLE `mistake` (
+  `id` int(11) NOT NULL,
+  `time` int(11) NOT NULL,
+  `success` int(11) NOT NULL,
+  `error` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Indexes for dumped tables
 --
@@ -62,6 +68,12 @@ ALTER TABLE `log_datas`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `mistake`
+--
+ALTER TABLE `mistake`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -69,7 +81,12 @@ ALTER TABLE `log_datas`
 -- AUTO_INCREMENT for table `log_datas`
 --
 ALTER TABLE `log_datas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1009;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1031;
+--
+-- AUTO_INCREMENT for table `mistake`
+--
+ALTER TABLE `mistake`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
