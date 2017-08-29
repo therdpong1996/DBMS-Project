97a0b79287c9328621b3a4832513b317850510f7
Therdpong Khangrang <numlifez@icloud.com>
2017-08-30 02:27:59 +0700

    Update Database.class.php

--- Database.class.php
+++ Database.class.php
@@ -12,17 +12,25 @@ class Database {
         $this->db_name = $db_name;
     }
 
-    public function loopdatabase($n = 20, $data){
+    public function loopdatabase($action, $n = 20, $data){
         $con = mysqli_connect($this->host , $this->username , $this->password , $this->db_name);
         mysqli_set_charset($con,"utf8");
-        $n_db = mysqli_fetch_row(mysqli_query($con, "SELECT COUNT(id) FROM log_datas"));
-        if($n_db[0] >= $n){
-            $del_n = $n_db[0] - $n + 1;
-            mysqli_query($con, "DELETE FROM log_datas ORDER BY id ASC LIMIT ".$del_n);
-            $this->insert($data);
-        }else{
-            $this->insert($data);
+
+        if ($action == 'normal') {
+            $n_db = mysqli_fetch_row(mysqli_query($con, "SELECT COUNT(id) FROM log_datas"));
+            if($n_db[0] >= $n){
+                $del_n = $n_db[0] - $n + 1;
+                mysqli_query($con, "DELETE FROM log_datas ORDER BY id ASC LIMIT ".$del_n);
+                $this->insert($data);
+            }else{
+                $this->insert($data);
+            }
+        }elseif ($action == 'mistake') {
+            echo $sql_m = "INSERT INTO mistake(success,error,time) VALUES ('".$data["success"]."', '".$data["error"]."', '".time()."')";
+            $save_m = mysqli_query($con, $sql_m);
+            mysqli_close($con);
         }
+
     }
 
     public function insert($data){
@@ -32,7 +40,7 @@ class Database {
         $sql_u = "INSERT INTO log_datas(temp,humi,time) VALUES ('".$data["temp"]."', '".$data["humi"]."', '".time()."')";
         $save_i = mysqli_query($con, $sql_i);
         $save_u = mysqli_query($con, $sql_u);
-        $con->close();
+        mysqli_close($con);
         if ($save_i and $save_u) {
             return true;
         }else{
