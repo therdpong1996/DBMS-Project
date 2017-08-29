b67e5c15463bb9622c984b3025e54624cad6e5e2
Therdpong Khangrang <numlifez@icloud.com>
2017-08-30 02:51:06 +0700

    Update esp8266-source.ino

--- esp8266-source/esp8266-source.ino
+++ esp8266-source/esp8266-source.ino
@@ -18,6 +18,10 @@ ESP8266WiFiMulti WiFiMulti;
 #define SECONDS_DS(seconds) ((seconds)*1000000UL)
 #define figger_p  "" //Figger Print your website if ues https
 
+int success = 0;
+int error = 0;
+int total = 0;
+
 String name;
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -44,10 +48,24 @@ void loop() {
   // Read temp & Humidity for DHT22
   float h = dht.readHumidity();
   float t = dht.readTemperature();
+    
+    Serial.println("Total" + total);
+    Serial.println("Success" + success);
+    Serial.println("Error" + error);
+  
+    if (isnan(h) || isnan(t)) {
+      Serial.println("Failed to read from DHT sensor!");
+      error++;
+      total++;
+      return;
+    }else{
+      success++;
+      total++;
+    }
   
     if ((WiFiMulti.run() == WL_CONNECTED)) {
       HTTPClient http;
-      String url = "https://<domain.com>/insert.php?temp=" + String(t) + "&humi=" + String(h); //Change <domain.com> to your domain
+      String url = "https://<domain.com>/insert.php?actin=normal&temp=" + String(t) + "&humi=" + String(h); //Change <domain.com> to your domain
       //if use http remove "s" form https://
       Serial.println(url);
       
@@ -62,25 +80,50 @@ void loop() {
       
       //http.begin(url); //if use HTTP
       http.begin(url,figger_p); //if use HTTPS
-      
-
       int httpCode = http.GET();
+      //check send data temp and humi
       if (httpCode > 0) {
         Serial.printf("[HTTP] GET... code: %d", httpCode);
         if (httpCode == HTTP_CODE_OK) {
           String payload = http.getString();
           Serial.println(payload);
         }else{
-          Serial.println("HTTP_ERROR");  
+          Serial.println("HTTP_ERROR");
         }
       } else {
         Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
       }
+      http.end();
+      
+      if(total == 100){
+          HTTPClient http_m;
+          String url_m = "https://<domain.com>/insert.php?actin=mistake&success=" + String(success) + "&error=" + String(error); //Change <domain.com> to your domain
+          Serial.println(url_m);
+          //http_m.begin(url_m); //if use HTTP
+          http_m.begin(url_m,figger_p); //if use HTTPS
+          int http_mCode = http_m.GET();
+          //check send data mistake
+          if (http_mCode > 0) {
+            Serial.printf("[HTTP_M] GET... code: %d", http_mCode);
+            if (http_mCode == HTTP_CODE_OK) {
+              String payload_m = http_m.getString();
+              Serial.println(payload_m);
+            }else{
+              Serial.println("HTTP_M_ERROR");
+            }
+          } else {
+            Serial.printf("[HTTP_M] GET... failed, error: %s\n", http.errorToString(http_mCode).c_str());
+          }
+          http_m.end();
+      }
         
-        http.end();          
-        delay(5000);                      //Send request every 5 seconds
-        Serial.println("Sleeping Mode ...");
-        ESP.deepSleep(SECONDS_DS(20));     //Sleep mode every 20 seconds
+      delay(5000);                    //Send request every 5 seconds
+      
+      if(total > 100){
+          Serial.println("Sleeping Mode ...");
+          ESP.deepSleep(SECONDS_DS(20));     //Sleep mode 20 seconds
+      }
+
     }else{
       Serial.println("WIFI Error");  
     }
